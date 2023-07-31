a7d4c9a35a4aa77d9847c5d3257d24120278a3fe
MOIN <muhammadmoin2526@gmail.com>
2023-07-31 00:42:12 -0700

    TodoList App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todolist, settodolist] = useState([]);
+  const [newtask, setnewtask] = useState("");
+
+  const change = (event) => {
+    setnewtask(event.target.value);
+  };
+
+  const add = () => {
+    const task = {
+      id: todolist.length === 0 ? 1 : todolist[todolist.length - 1].id + 1,
+      taskname: newtask,
+      done: false,
+    };
+    settodolist([...todolist, task]);
+  };
+
+  const remove = (taskname) => {
+    settodolist(todolist.filter((task) => task != taskname));
+  };
+
+  const done = (id) => {
+    settodolist(
+      todolist.map((task) => {
+        if (task.id === id) {
+          return { ...task, done: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TodoList App</h1>
+      <div>
+        <input onChange={change}></input>
+        <button onClick={add}>Add Task</button>
+        <div className="task">
+          {todolist.map((task) => {
+            return (
+              <div className="todolist" style={{ backgroundColor: task.done ? "rgb(124, 252, 0)" : "white" }}>
+                <h1 contentEditable>{task.taskname}</h1>
+                <button className="done" onClick={() => done(task.id)}>Done</button>
+                <button className="remove" onClick={() => remove(task)}>X</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
